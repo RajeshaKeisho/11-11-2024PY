eb7f76df542d61ec112f3588908a10094480692d
Rajesha <rajarajesha@gmail.com>
2025-02-27 18:50:45 +0530

    'sfo'

---
+++ 26 Search Filter and Ordering/ecommerce_project/products/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from typing import Any 
+from .models import Product
+from .serializers import ProductSerializer
+from .filters import ProductFilter
+import django_filters
+from django.views.generic import ListView
+from rest_framework import generics, filters
+
+# Create your views here.
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+
+    filterset_class = ProductFilter
+    search_fields = ['name', 'description']
+    ordering_fields = ['name', 'price', 'stock']
+    ordering = ['name']
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/products_list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filter = ProductFilter(self.request.GET, queryset=queryset)
+        queryset = self.filter.qs
+
+        # Apply ordering
+        ordering = self.request.GET.get('ordering', 'name')
+        queryset = queryset.order_by(ordering)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = self.filter
+        return context
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
