4c5bedbdf9586c74001f6f257961e38092c123cd
Rajesha <rajarajesha@gmail.com>
2025-03-14 23:28:34 +0530

    'modelinheritance'

---
+++ 9A Model Inheritance/organization/people/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Employee, Customer
+
+# admin.site.register(Person)
+admin.site.register(Employee)
+admin.site.register(Customer)
+# admin.site.register(Product)
+# admin.site.register(ProductPrice)
+
